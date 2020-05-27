d85681d42bc2b122a27148bc0e0164f4caedf909
grep-safi <hub.of.safi@gmail.com>
2020-05-27 12:27:47 -0700

    Added code that draws high resolution sphere

--- src/Orbit.js
+++ src/Orbit.js
@@ -1,92 +1,75 @@
-// If you change width or height, you must change the css width and height for div,
-// the aspect ratio in this file and of course, the width and height values in the canvas element
+let canvas,
+    camera,
+    scene,
+    renderer;
 
-import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r115/build/three.module.js';
-
-let camera,
-    scene;
-
-function createCamera(width, height) {
+function createCamera() {
     const fov = 75;
-    const aspect = width / height;
+    const aspect = canvas.width / canvas.height;
     const near = 0.1;
     const far = 5;
 
     camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2;
+    camera.position.z = 5;
 }
 
-function main() {
-    const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+function createSphere() {
+    const radius = 0.75;
+    const widthSegments = 100;
+    const heightSegments = 100;
+    const sphereGeometry = new THREE.SphereBufferGeometry(
+        radius, widthSegments, heightSegments);
 
-    createCamera(canvas.clientWidth, canvas.clientHeight);
+    const solarSystem = new THREE.Object3D();
+    scene.add(solarSystem);
 
-    scene = new THREE.Scene();
+    const sunMaterial = new THREE.MeshPhongMaterial();
+    // const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xffff6b});
+    const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+    solarSystem.add(sunMesh);
+}
 
+function resizeRendererToDisplaySize(renderer) {
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
+
+function createLight() {
     const color = 0xFFFFFF;
-    const intensity = 1;
+    const intensity = 0.60;
     const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
+    light.position.set(0,0,1);
     scene.add(light);
+}
 
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+function init() {
+    canvas = document.querySelector('#c');
+    renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+    scene = new THREE.Scene();
+    createCamera();
 
-    function makeInstance(geometry, color, x) {
-        const material = new THREE.MeshPhongMaterial({color});
+    createLight();
 
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-
-        cube.position.x = x;
-
-        return cube;
-    }
-
-    const cubes = [
-        makeInstance(geometry, 0x44aa88,  0),
-        makeInstance(geometry, 0x8844aa, -2),
-        makeInstance(geometry, 0xaa8844,  2),
-    ];
-
-    function resizeRendererToDisplaySize(renderer) {
-        const canvas = renderer.domElement;
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        const needResize = canvas.width !== width || canvas.height !== height;
-        if (needResize) {
-            renderer.setSize(width, height, false);
-        }
-        return needResize;
-    }
-
-    function render() {
-
-        if (resizeRendererToDisplaySize(renderer)) {
-            const canvas = renderer.domElement;
-            camera.aspect = canvas.clientWidth / canvas.clientHeight;
-            camera.updateProjectionMatrix();
-        }
-
-        cubes.forEach((cube, ndx) => {
-            const speed = 0.01 + ndx * 0.01;
-            const rot = speed;
-            cube.rotation.x += rot;
-            cube.rotation.y += rot;
-        });
-
-        renderer.render(scene, camera);
-
-        requestAnimationFrame(render);
-    }
+    createSphere();
 
     render();
-
-    // requestAnimationFrame(render);
 }
 
-main();
+function render() {
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+
+init();
 
